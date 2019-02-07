1961a1bb02d092fb77c5749397f00f41e6b55ca5
Unknown <lucafloreau@akeonet.com>
2019-02-07 15:37:48 +0100

    test

---
+++ TimeEater/Assets/Scripts/New Folder/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Enemy
+{
+	[SerializeField] private float horizontalSpeed = 5.0f;
+	[SerializeField] private float frequency = 1.0f;
+	[SerializeField] private float amplitude = 0.0f;
+
+	private Vector3 initialAxis;
+	private Vector3 initialPosition;
+	private float timer = 0.0f;
+
+	private new void Start()
+	{
+		base.Start();
+		initialPosition = transform.position;
+		initialAxis = transform.right;
+	}
+
+	private void Update()
+	{
+		if (canMove)
+		{
+			SinMove();
+			timer += Time.deltaTime;
+		}
+	}
+	
+	private void SinMove()
+	{
+		initialPosition += transform.up * Time.deltaTime * horizontalSpeed;
+		transform.position = initialPosition + initialAxis * Mathf.Sin(timer * frequency) * amplitude;
+	}
+}
---
+++ TimeEater/Assets/Scripts/New Folder/Cloud.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Cloud : BasicObject
+{
+	[SerializeField] private Sprite[] sprites;
+	[SerializeField] private float speed = 10;
+	[SerializeField] private float rangeSpeed = 2;
+
+	private SpriteRenderer mySpriteRender;
+
+
+	private new void Start()
+	{
+		base.Start();
+		mySpriteRender = GetComponentInChildren<SpriteRenderer>();
+
+		ChangeSprite(Random.Range(0, sprites.Length - 1));
+		speed += Random.Range(-rangeSpeed, rangeSpeed);
+
+		//objects closer seem to be moving faster
+		mySpriteRender.sortingOrder = (int) speed;
+	}
+
+	private void Update()
+	{
+		transform.position = transform.position + Vector3.down * speed * Time.deltaTime;
+	}
+
+	//ChangeSprite based on its index
+	public void ChangeSprite(int index)
+	{
+		if (index < sprites.Length && index >= 0)
+		{
+			mySpriteRender.sprite = sprites[index];
+		}
+		else
+		{
+			Debug.Log("ChangeSprite : wrong index for cloud sprite entered");
+		}
+	}
+}
