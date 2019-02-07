2bed0869f390b2b6630f24fb05c2415cdaeafb91
Unknown <lucafloreau@akeonet.com>
2019-02-07 10:00:56 +0100

    alpha commit

---
+++ TimeEater/Assets/Scripts/CandleAgainstThePaint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class CandleAgainstThePaint : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private Transform candleRespawn;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Paint"))
+        {
+            door.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        
+        else if (col.gameObject.CompareTag("Wall"))
+        {
+            col.transform.position = candleRespawn.position;
+        }
+    }
+}
---
+++ TimeEater/Assets/Scripts/GoToMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+    {
+
+        public void LoadMenu()
+        {
+            Time.timeScale = 1.0f;
+            SceneManager.LoadScene("Menu");
+        }
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+}
